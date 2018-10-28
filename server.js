a4e3fa75405b3dae304847af9cc34a8a32c55076
Makiko Vaughan <makikovaughan@Makikos-MBP.attlocal.net>
2018-10-27 23:01:34 -0400

    Created index.html

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const app = express();
 //Port setup
 var PORT = process.env.PORT || 3000;
 
+// Sets up our server to parse our request body for usage
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "app/public")));
+
 //Routing
 require('./app/routing/apiRoutes.js')(app);
 require('./app/routing/htmlRoutes.js')(app);
