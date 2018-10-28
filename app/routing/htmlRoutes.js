8579fcc6be3b8dc24d1ea0c37935f7eae189ca66
Makiko Vaughan <makikovaughan@Makikos-MBP.attlocal.net>
2018-10-28 15:37:23 -0400

    Updated file path

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,14 +5,14 @@ const path = require('path');
 module.exports = function (app) {
 
 
+    //Go to survey page
+    app.get('/survey', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/survey.html'));
+    });
+
     // Go to home.html to be a default.
     app.get('*', function (req, res) {
         res.sendFile(path.join(__dirname, '../public/home.html'));
-    });
-
-    //Go to survey page
-    app.get('/survey', function (req, res) {
-        res.sendFile(path.join(__dirname, '../app/public/survey.html'));
     });
 
 }
